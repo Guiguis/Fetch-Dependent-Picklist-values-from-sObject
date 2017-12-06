1c79ae4d41b0493220e59e3bbffe146009964999
Guilherme Miranda <guilhermealvesmiranda99@gmail.com>
2017-12-05 22:31:52 -0200

    Adiciona código para lidar com Picklists

---
+++ LC - Dependent PickList/Single PickList Values.java
@@ -0,0 +1,30 @@
+/**
+* Pega os valores do PickList 
+* 
+* @param  objObject sObjectType
+* @param  fld       String     Campo  Linha atual da peça a ser movida
+* @return allOpts   List       Lista com as oções da lista do Picklist de Nível 1
+*/    
+@AuraEnabled
+public static List <String> getselectOptions(sObject objObject, string fld) {
+    List < String > allOpts = new list < String > ();
+        // Get the object type of the SObject.
+    Schema.sObjectType objType = objObject.getSObjectType();
+
+        // Describe the SObject using its object type.
+    Schema.DescribeSObjectResult objDescribe = objType.getDescribe();
+
+        // Get a map of fields for the SObject
+    map < String, Schema.SObjectField > fieldMap = objDescribe.fields.getMap();
+
+        // Get the list of picklist values for this field.
+    list < Schema.PicklistEntry > values =
+    fieldMap.get(fld).getDescribe().getPickListValues();
+
+        // Add these values to the selectoption list.
+    for (Schema.PicklistEntry a: values) {
+        allOpts.add(a.getValue());
+    }
+    allOpts.sort();
+    return allOpts;
+}
